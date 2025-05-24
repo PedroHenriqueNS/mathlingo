707fb8f750ba916a9b45605bc38d7362f251b385
Pedro Henrique <pedrohe.contato@gmail.com>
2025-05-24 10:43:26 -0300

    Create activities.ts

---
+++ constants/activities.ts
@@ -0,0 +1,77 @@
+export const activitiesList: TActivityConst[] = [
+  {
+    id: 1,
+    title: "Conteúdo 1",
+    teoricalContent: [ //// PÁGINAS
+      {
+        id: 0,
+        contentTitle: "Explicação 1",
+        paragraphs: [ //// PARAGRAFOS
+          {
+            id: 0,
+            type: "Texto",
+            text: "Texto de teste 21321321 dsasdsadsadvcx, segue abaixo uma formula matemateca"
+          },
+          {
+            id: 1,
+            type: "Math",   //// FORMULAS MATEMÁTICAS EM "EXPRESSÕES DE LaTeX" (LaTeX é uma expressão matematica ou formula que é formatada e exibida em um documento digital)
+            text: 'x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}'
+          },
+          {
+            id: 2,
+            type: "Texto-Math",
+            text: `This text includes math notations and should be wrapped correctly for \\( \\alpha \\) and $\\beta$ within the view. \nThe following formula shouldn't be inline:$$x_{1,2} = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}$$However the following formula should be inline with the text: \\( a^2 + b^2 = c^2 \\)`
+          }
+        ]
+      },
+      {
+        id: 1,
+        contentTitle: "Explicação 2",
+        paragraphs: [ //// PARAGRAFOS
+          {
+            id: 0,
+            type: "Texto",
+            text: "Texto de teste 21321321 dsasdsadsadvcx, segue abaixo uma formula matemateca"
+          },
+          {
+            id: 1,
+            type: "Texto-Math",
+            text: `This text includes math notations and should be wrapped correctly for \\( \\alpha \\) and $\\beta$ within the view. \nThe following formula shouldn't be inline:$$x_{1,2} = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}$$However the following formula should be inline with the text: \\( a^2 + b^2 = c^2 \\)`
+          }
+        ]
+      }
+    ],
+    questionContent: [ //// PÁGINAS
+      {
+        id: 0,
+        contentTitle: "Atividade 1",
+        paragraphs: [ //// PARAGRAFOS DO TEXTO
+          {
+            id: 0,
+            type: "Texto",
+            text: "Assinale a resposta correta da seguinte fórmula"
+          },
+          {
+            id: 1,
+            type: "Math",
+            text: 'x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}'
+          }
+        ],
+        alternatives: [
+          {
+            id: 0,
+            type: "Math",
+            isAlternativaCerta: true,
+            text: 'x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}'
+          },
+          {
+            id: 1,
+            type: "Math",
+            isAlternativaCerta: false,
+            text: 'x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}'
+          },
+        ],
+      },
+    ]
+  },
+]

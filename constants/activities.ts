76a39744c2eb97f52fde234023d1bb023a4d4b52
Letícia Matheus <let.matheus19@hotmail.com>
2025-05-26 21:52:17 -0300

    Adicionando o conteúdo de funções

--- constants/activities.ts
+++ constants/activities.ts
@@ -75,4 +75,178 @@ export const activitiesList: TActivityConst[] = [
       },
     ],
   },
+  {
+    id: 2,
+    title: "Relações e Funções",
+    achievementSlugs: ['achievement-test'],
+    teoricalContent: [
+      {
+        id: 0,
+        contentTitle: "Função Polinomial do 1° Grau",
+        paragraphs: [
+          {
+            id: 0,
+            type: "Texto",
+            text: "Uma função é chamada função afim ou função polinomial do 1º grau quando existem dois números reais a e b tais que:"
+          },
+          {
+            id: 1,
+            type: "Math",
+            text: 'f(x)=ax+b'
+          },
+          {
+            id: 2,
+            type: "Texto",
+            text: "Se a = 0, a função é constante. Quando a > 0, a função é decrescente. E, por fim, quando a < 0, a função é decrescente."
+          }
+        ]
+      },
+      {
+        id: 1,
+        "contentTitle": "Função Polinomial do 2° Grau",
+        paragraphs: [
+          {
+            id: 0,
+            type: "Texto",
+            text: "Uma função é chamada função quadrática ou função polinomial do 2º grau quando existem três números reais a, b e c, tais que:"
+          },
+          {
+            id: 1,
+            type: "Math",
+            text: 'f(x)=ax^2+bx+c'
+          },
+          {
+            id: 2,
+            type: "Texto",
+            text: "É importante constar que a deve ser diferente de 0, se não a função se torna de 1º grau."
+          },
+          {
+            id: 3,
+            type: "Texto",
+            text: "O a também é responsável pela abertura e concavidade da parábola. Se ele for positivo, a concavidade é para cima, e se for negativo, a concavidade fica para baixo. Além disso, quanto maior o valor de a, menor é a abertura da parábola."
+          },
+          {
+            id: 4,
+            type: "Texto",
+            text: "Enquanto isso, o b indica o ramo de intersecção com o eixo y. Se b > 0, a intersecção é no ramo crescente, se b < 0 a intersecção é no ramo decrescente, e se b = 0 a intersecção é no vértice."
+          },
+          {
+            id: 5,
+            type: "Texto",
+            text: "Por fim, o c define o ponto de intersecção com o eixo y. Ou seja, f(0) = c."
+          },
+          {
+            id: 6,
+            type: "Texto",
+            text: "Com a Fórmula de Bháskara conseguimos determinar as raízes de uma equação de 2º grau, ou seja, os pontos de intersecção da parábola com o eixo x. Sua fórmula é:"
+          },
+          {
+            id: 7,
+            type: "Math",
+            text: 'x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}'
+          },
+          {
+            id: 8,
+            type: "Texto",
+            text: "O cálculo de delta (Δ), essencial para a fórmula anterior e as próximas, auxilia a descobrirmos quantas vezes a parábola corta o eixo x. Se Δ > 0 há dois pontos, se Δ = 0 há somente um ponto, e se Δ < 0 não há nenhum ponto"
+          },
+          {
+            id: 9,
+            type: "Math",
+            text: '\\Delta=b^2-4ac'
+          },
+          {
+            id: 10,
+            type: "Texto",
+            text: "Para acharmos o ponto do vértice da parábola, ou seja, seu ponto mais alto ou mais baixo, podemos usar as fórmulas a seguir:"
+          },
+          {
+            id: 11,
+            type: "Math",
+            text: 'x_v=\\frac{-b}{2a}'
+          },
+          {
+            id: 12,
+            type: "Math",
+            text: 'y_v=\\frac{-\\Delta}{4a}'
+          }
+        ]
+      },
+      {
+        id: 2,
+        "contentTitle": "Função Exponencial",
+        paragraphs: [
+          {
+            id: 0,
+            type: "Texto",
+            text: "Dado um número a, positivo e diferente de 1, uma função exponencial de base a é definida como:"
+          },
+          {
+            id: 1,
+            type: "Math",
+            text: 'f(x)=a^x'
+          },
+          {
+            id: 2,
+            type: "Texto",
+            text: "Há algumas restrições. O número a não pode ser igual a 0 ou 1, e negativo. Já x não pode ser igual a 1/2."
+          },
+          {
+            id: 3,
+            type: "Texto",
+            text: "Em relação ao gráfico, essa função corta o eixo y em b, não corta o eixo x nunca e é ilimitada superiormente. Além disso, se a > 1 a função é crescente, e se 0 < a < 1 a função é decrescente."
+          },
+          {
+            id: 4,
+            type: "Texto",
+            text: "Um dos maiores usos dessa função é representado no exemplo da Fórmula Financeira a seguir. Nela, V é o valor acumulado após o tempo t, V0 é o valor inicial e i é a taxa, podendo ser negativa ou positiva."
+          },
+          {
+            id: 5,
+            type: "Math",
+            text: 'V=V_0(1+i)^t'
+          }
+        ]
+      },
+      {
+        id: 3,
+        "contentTitle": "Função Logarítmica",
+        paragraphs: [
+          {
+            id: 0,
+            type: "Texto",
+            text: "A função logarítmica dá-se por:"
+          },
+          {
+            id: 1,
+            type: "Math",
+            text: 'f(x)=\\log_{a}{x}'
+          },
+          {
+            id: 2,
+            type: "Texto",
+            text: "O número a deve ser positivo e diferente de 1. Se a > 1 a função é decrescente, e se 0 < a < 1 a função é decrescente. Além disso, ela nunca toca o eixo y."
+          },
+          {
+            id: 3,
+            type: "Texto",
+            text: "Abaixo há duas propriedades logarítmicas importantes."
+          },
+          {
+            id: 4,
+            type: "Math",
+            text: '\\log_{a}b = x \\iff a^{x} = b'
+          },
+          {
+            id: 5,
+            type: "Math",
+            text: '\\log_{a}(b^{c}) = c \\cdot \\log_{a}b'
+          }
+        ]
+      }
+    ],
+    questionContent: [
+      //Vou adicionar as questões depois
+    ]
+  }
 ];

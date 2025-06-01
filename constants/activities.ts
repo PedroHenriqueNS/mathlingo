a07bef6f8aba323ca2ac3366d77c25a714fce96c
Letícia Matheus <let.matheus19@hotmail.com>
2025-06-01 16:55:41 -0300

    Adicionando as questões de funções

--- constants/activities.ts
+++ constants/activities.ts
@@ -77,7 +77,7 @@ export const activitiesList: TActivityConst[] = [
   },
   {
     id: 2,
-    title: "Relações e Funções",
+    title: "Funções",
     achievementSlugs: ['achievement-test'],
     teoricalContent: [
       {
@@ -246,7 +246,279 @@ export const activitiesList: TActivityConst[] = [
       }
     ],
     questionContent: [
-      //Vou adicionar as questões depois
+      {
+        id: 0,
+        contentTitle: 'Atividade 1',
+        paragraphs: [
+          {
+            id: 0,
+            type: 'Texto',
+            text: 'O custo de fabricação de x unidades de um produto é dado pela função C(x) = 100 + 2x. Qual o custo de fabricação da décima unidade, já tendo sido fabricadas nove unidades?',
+          }
+        ],
+        alternatives: [
+          {
+            id: 0,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: 'R$120,00',
+          },
+          {
+            id: 1,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: 'R$118,00',
+          },
+          {
+            id: 2,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: 'R$100,00',
+          },
+          {
+            id: 3,
+            type: 'Texto',
+            isAlternativaCerta: true,
+            text: 'R$2,00',
+          },
+          {
+            id: 4,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: 'R$10,00',
+          },
+        ],
+      },
+      {
+        id: 1,
+        contentTitle: 'Atividade 2',
+        paragraphs: [
+          {
+            id: 0,
+            type: 'Texto',
+            text: 'Duas editoras oferecem emprego com as seguintes condições salariais. Empresa A – Fixo de R$ 1.500,00 e comissão de R$ 15,00 por coleção vendida. Empresa B – Fixo de R$ 1.200,00 e comissão de R$ 20,00 por coleção vendida. Qual a quantidade de coleções vendidas em que o salário das duas empresas será o mesmo?',
+          }
+        ],
+        alternatives: [
+          {
+            id: 0,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: '35 coleções',
+          },
+          {
+            id: 1,
+            type: 'Texto',
+            isAlternativaCerta: true,
+            text: '50 coleções',
+          },
+          {
+            id: 2,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: '60 coleções',
+          },
+          {
+            id: 3,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: '75 coleções',
+          },
+          {
+            id: 4,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: '80 coleções',
+          },
+        ],
+      },
+      {
+        id: 2,
+        contentTitle: 'Atividade 3',
+        paragraphs: [
+          {
+            id: 0,
+            type: 'Texto',
+            text: 'Sabe-se que, sob certo ângulo de tiro, a altura h atingida por uma bala, em metros, em função do tempo t, em segundos, é dada por h(t) = -20t2 + 200t. Qual a altura máxima atingida pela bala? Em quanto tempo, após o tiro, a bala atinge a altura máxima?',
+          }
+        ],
+        alternatives: [
+          {
+            id: 0,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: 'A altura máxima é de 350 metros, atingida após 2 segundos',
+          },
+          {
+            id: 1,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: 'A altura máxima é de 400 metros, atingida após 3.5 segundos',
+          },
+          {
+            id: 2,
+            type: 'Texto',
+            isAlternativaCerta: true,
+            text: 'A altura máxima é de 500 metros, atingida após 5 segundos',
+          },
+          {
+            id: 3,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: 'A altura máxima é de 550 metros, atingida após 6 segundos',
+          },
+          {
+            id: 4,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: 'A altura máxima é de 620 metros, atingida após 7.5 segundos',
+          },
+        ],
+      },
+      {
+        id: 3,
+        contentTitle: 'Atividade 4',
+        paragraphs: [
+          {
+            id: 0,
+            type: 'Texto',
+            text: 'A trajetória de um projétil lançado por um canhão, num local plano e horizontal, é dada pela função:',
+          },
+          {
+            id: 1,
+            type: 'Math',
+            text: 'y = -\\frac{1}{32} \\cdot x^{2} + \\frac{1}{8} \\cdot x',
+          },
+          {
+            id: 2,
+            type: 'Texto',
+            text: 'A que distância caiu o projétil?',
+          },
+        ],
+        alternatives: [
+          {
+            id: 0,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: '8,5 quilômetros',
+          },
+          {
+            id: 1,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: '10 quilômetros',
+          },
+          {
+            id: 2,
+            type: 'Texto',
+            isAlternativaCerta: true,
+            text: '12 quilômetros',
+          },
+          {
+            id: 3,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: '14,5 quilômetros',
+          },
+          {
+            id: 4,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: '20 quilômetros',
+          },
+        ],
+      },
+      {
+        id: 4,
+        contentTitle: 'Atividade 5',
+        paragraphs: [
+          {
+            id: 0,
+            type: 'Texto',
+            text: 'O número de habitantes de uma cidade é hoje igual a 7.000 e cresce a uma taxa de 3% ao ano. Qual o número de habitantes daqui a 30 anos?',
+          }
+        ],
+        alternatives: [
+          {
+            id: 0,
+            type: 'Texto',
+            isAlternativaCerta: true,
+            text: '16.990 habitantes',
+          },
+          {
+            id: 1,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: '17.100 habitantes',
+          },
+          {
+            id: 2,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: '21.500 habitantes',
+          },
+          {
+            id: 3,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: '24.050 habitantes',
+          },
+          {
+            id: 4,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: '32.750 habitantes',
+          },
+        ],
+      },
+      {
+        id: 5,
+        contentTitle: 'Atividade 6',
+        paragraphs: [
+          {
+            id: 0,
+            type: 'Texto',
+            text: 'Considere que uma determinada xícara de café contém cerca de 100mg de cafeína. O nível Q de cafeína no corpo decresce a uma taxa de 7% por hora, a partir do instante em que o café foi tomado. Considerando o modelo matemático exponencial abaixo, determine após quanto tempo o nível de cafeína atingirá a meia vida.',
+          },
+          {
+            id: 1,
+            type: 'Math',
+            text: 'Q=Q_0(1+i)^t',
+          }
+        ],
+        alternatives: [
+          {
+            id: 0,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: 'Após 3 horas',
+          },
+          {
+            id: 1,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: 'Após 4 horas e meia',
+          },
+          {
+            id: 2,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: 'Após 6 horas e 45 minutos',
+          },
+          {
+            id: 3,
+            type: 'Texto',
+            isAlternativaCerta: false,
+            text: 'Após 8 horas',
+          },
+          {
+            id: 4,
+            type: 'Texto',
+            isAlternativaCerta: true,
+            text: 'Após 9 horas e meia',
+          },
+        ],
+      },
     ]
   }
 ];
